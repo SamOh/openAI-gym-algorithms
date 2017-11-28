b3ea1b421574dcecc0b2905ed5a4325e1cdddbff
Brandon Hills <brandon_w_hills@college.harvard.edu>
2017-11-28 18:41:09 -0500

    making sure main.py doesn't conflict with master so I can pull

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
-import sys
 import gym
+import os
+import utils
+from learningAgents import HillClimbingAgent, SimulatedAnnealingAgent, RandomAgent
 
-# do stuff
+### User Params ###
+
+# The name of the game to solve
+game_name = 'SpaceInvaders-v0'
+number_iterations = 100
+number_trials = 5
+# 6 is for space invaders. Check the number potential for each game by printing action_space
+number_potential_actions = 6
+
+### End User Params ###
+
+hc_agent = HillClimbingAgent(game_name, number_iterations, number_potential_actions)
+random_agent = RandomAgent(game_name, number_iterations, number_potential_actions)
+
+# train agent here and save results of the best
+# print(hc_agent.train_agent())
+print(random_agent.train_agent())

75940dfc450f0dd26086ad13ca3fc2de5a7da2a4
Collin Price <collinprice@CollinsMacbookPro.local>
2017-12-01 16:53:23 -0500

    taxi search and agent rearranged

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import gym
-import os
 import utils
 from learningAgents import *
 
@@ -8,9 +7,9 @@ from learningAgents import *
 # The name of the game to solve
 game_name = 'Taxi-v2'
 number_iterations = 2000
-epsilon = 0.05
+epsilon = 0.15
 gamma = 0.9
-alpha = 0.5
+alpha = 0.1
 
 tdl_agent = TDLearningAgent(game_name, number_iterations, epsilon, gamma, alpha)
 tdl_agent.train_agent()

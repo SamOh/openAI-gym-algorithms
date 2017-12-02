ce7e453b319be4c2733f42875fdd107ae17d3223
Sammy Oh <soh02@college.harvard.edu>
2017-12-02 11:45:42 -0500

    premerge

--- learningAgents.py
+++ learningAgents.py
@@ -1,6 +1,7 @@
 import gym
 import random
 import utils
+import numpy as np
 from taxi import solveTaxi
 
 """
@@ -127,25 +128,86 @@ class MonteCarloAgent(LearningAgent):
         return
 
 class DiscreteInputBasicQLearning(Agent):
-    pass
-    # for episode in range(self.episodes):
-    #     self.observation = self.env.reset()
-    #     done = False
-    #     total_reward, reward = 0,0
+    def __init__(self, game_name, iterations, epsilon, gamma, alpha):
+        self.env = gym.make(game_name)
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.alpha = alpha
+        self.iterations = iterations
+        self.QValues = np.zeros([env.observation_space.n,env.action_space.n])
+        self.actions = [i for i in range(self.env.action_space.n)]
 
-    #     action = numpy.argmax(self.Q[self.observation])
+    def getQValue(self, state, action):
+        return self.QValues[state, action]
 
-    #     observation, reward, done, info = self.env.step(self.env.action_space.sample())
-    #     Q[self.observation, action] += alpha * (reward + gamma * (numpy.max(Q[observation] - Q[self.observation, action])))
-    #     self.observation = observation
-    #     total_reward += reward
+    def getValue(self, state):
+        maxQValue = None
+        for action in self.actions:
+          QValue = self.getQValue(state, action)
+          if maxQValue < QValue:
+            maxQValue = QValue
+        return 0 if maxQValue is None else maxQValue
 
+    def getPolicy(self, state):
+        maxQValue, maxAction = None, None
+        for action in self.actions:
+          QValue = self.getQValue(state, action)
+          if maxQValue < QValue:
+            maxQValue = QValue
+            maxAction = action
+        return maxAction
 
-"""
-QLearning agent (RL2 last thing scott talked about)
-"""
-class ApproximateQLearningAgent(Agent):
-    def __init__(self, game_name, iterations):
-        # instantiate Q values
-        pass
+    def epsilonGreedyAction(self, state):
+        return random.choice(self.actions) if utils.flipCoin(self.epsilon) else self.getPolicy(state)
+
+    def updateQValues(self, state, action, nextState, reward):
+        self.QValues[state, action] = (1 - self.alpha) * self.QValues[state, action] + \
+          self.alpha * (reward + self.gamma * self.getValue(nextState))
+
+    def train_agent(self):
+        print 'training TDLearningAgent with {} iterations...'.format(self.iterations)
+        for episode in range(self.iterations):
+            episode_rewards = 0
+            done, prevObs = False, self.env.reset()
+            while not done:
+                action = self.epsilonGreedyAction(prevObs)
+                obs, reward, done, _ = self.env.step(action)
+                self.updateQValues(prevObs, action, obs, reward)
+                prevObs = obs
+                episode_rewards += reward
+            #print 'training episode gained {} rewards in episode {}'.format(episode_rewards, episode)
+
+    def test_agent(self):
+        print 'testing TDLearningAgent...'
+        passed = 0
+        for _ in range(self.iterations):
+            obs, done, actions = self.env.reset(), False, []
+            optimalActions = solveTaxi(obs)
+            while not done:
+                action = self.getPolicy(obs)
+                actions.append(action)
+                obs, _, done, _ = self.env.step(action)
+                if len(actions) > len(optimalActions):
+                    break
+            if done:
+                if len(actions) == len(optimalActions):
+                    passed += 1
+                if len(actions) < len(optimalActions):
+                    print "This is impossible!"
+        print '{}% of tests passed optimally'.format(passed * 100.0 / self.iterations)
+
+    # def run(self):
+    #     for episode in range(self.episodes):
+    #         self.observation = self.env.reset()
+    #         done = False
+    #         total_reward, reward = 0,0
+
+    #         action = numpy.argmax(self.Q[self.observation])
+
+    #         observation, reward, done, info = self.env.step(self.env.action_space.sample())
+    #         Q[self.observation, action] += alpha * (reward + gamma * (numpy.max(Q[observation] - Q[self.observation, action])))
+    #         self.observation = observation
+    #         total_reward += reward
+
+    #     print "Score over time: " +  str(sum(rList)/num_episodes)
 
--- main.py
+++ main.py
@@ -11,9 +11,11 @@ epsilon = 0.15
 gamma = 0.9
 alpha = 0.1
 
-tdl_agent = TDLearningAgent(game_name, number_iterations, epsilon, gamma, alpha)
-tdl_agent.train_agent()
-tdl_agent.test_agent()
+# tdl_agent = TDLearningAgent(game_name, number_iterations, epsilon, gamma, alpha)
+# tdl_agent.train_agent()
+# tdl_agent.test_agent()
 
 # random_agent = RandomAgent(game_name)
 # random_agent.test_agent()
+
+qlearning = DiscreteInputBasicQLearning(game_name, number_iterations, epsilon, gamma, alpha)
